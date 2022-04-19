0aba230ddc35918dd5559d18960c7bc6a64fb051
Priyanka <priyankahansi95@gmail.com>
2022-04-20 00:44:30 +0530

    pattern programs added

---
+++ src/com/Numberpattern/Increasingnum.java
@@ -0,0 +1,15 @@
+package com.Numberpattern;
+
+public class Increasingnum {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            int p = 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(p++ + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
